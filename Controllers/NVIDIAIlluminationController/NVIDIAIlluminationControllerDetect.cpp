eea1b1e7e9c0763ae24d8eba0885852bad3392d3
cmill <cmill@DESKTOP-RO96E3R>
2022-05-24 21:01:09 -0400

    Wrapping Nvidia files in #ifdefs to try to prevent compilation, removed more surgical #ifdefs, added debug prints.

--- Controllers/NVIDIAIlluminationController/NVIDIAIlluminationControllerDetect.cpp
+++ Controllers/NVIDIAIlluminationController/NVIDIAIlluminationControllerDetect.cpp
@@ -68,8 +68,10 @@ void DetectNVIDIAIlluminationGPUControllers(std::vector<i2c_smbus_interface*>& b
     //LOG_DEBUG("Entered detection function.");
     for (unsigned int bus = 0; bus < busses.size(); bus++)
     {
+        LOG_DEBUG("Inside outer for loop.");
         for(unsigned int dev_idx = 0; dev_idx < GPU_NUM_DEVICES; dev_idx++)
         {
+            LOG_DEBUG("Inside inner for loop.");
             if (busses[bus]->port_id != 1)
             {
                 break;
@@ -80,6 +82,7 @@ void DetectNVIDIAIlluminationGPUControllers(std::vector<i2c_smbus_interface*>& b
                busses[bus]->pci_subsystem_vendor == device_list[dev_idx].pci_subsystem_vendor &&
                busses[bus]->pci_subsystem_device == device_list[dev_idx].pci_subsystem_device)
             {
+                LOG_DEBUG("Outside switch statement.");
                 switch(device_list[dev_idx].gpu_rgb_version)
                 {
                     case NVIDIA_ILLUMINATION_V1:
--- Controllers/NVIDIAIlluminationController/NVIDIAIlluminationV1Controller.h
+++ Controllers/NVIDIAIlluminationController/NVIDIAIlluminationV1Controller.h
@@ -1,3 +1,4 @@
+#ifdef _WIN32
 /*-----------------------------------------*\
 |  NVIDIAIlluminationV1Controller.h         |
 |                                           |
